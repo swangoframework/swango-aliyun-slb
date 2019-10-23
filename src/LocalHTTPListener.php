9696ee2df0ffa81e01899ff47cc9b6244ed63abc
Tree <tree@brea.co>
2019-10-23 14:57:25 +0800

    bug fix

--- src/LocalHTTPListener.php
+++ src/LocalHTTPListener.php
@@ -7,15 +7,19 @@ class LocalHTTPListener {
     const BALANCER_LISTENER_PORT = 80;
     const STATUS_STARTING = 'starting', STATUS_RUNNING = 'running', STATUS_CONFIGURING = 'configuring';
     const STATUS_STOPPING = 'stopping', STATUS_STOPPED = 'stopped', STATUS_NONE = 'none';
-    public static function isAvailable(): bool {
+    public static function isAvailable(bool $auto_build = true): bool {
         $describe_action = new DescribeLoadBalancerHTTPListenerAttribute();
         switch ($describe_action->getResult()->Status) {
             case self::STATUS_NONE:
-                $create_action = new CreateLoadBalancerHTTPListener();
-                $create_action->getResult();
-                $start_action = new StartLoadBalancerListener();
-                $start_action->getResult();
-                return true;
+                if ($auto_build) {
+                    $create_action = new CreateLoadBalancerHTTPListener();
+                    $create_action->getResult();
+                    $start_action = new StartLoadBalancerListener();
+                    $start_action->getResult();
+                    return true;
+                } else {
+                    return false;
+                }
             case self::STATUS_STARTING:
             case self::STATUS_RUNNING:
                 return true;
