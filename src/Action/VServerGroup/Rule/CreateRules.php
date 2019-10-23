fdf5e07ebf651b5864c6f4ed904f8fb72de4c9f6
Tree <tree@brea.co>
2019-10-23 15:03:27 +0800

    bug fix

--- src/Action/VServerGroup/Rule/CreateRules.php
+++ src/Action/VServerGroup/Rule/CreateRules.php
@@ -2,10 +2,12 @@
 namespace Swango\Aliyun\Slb\Action\VServerGroup\Rule;
 use Swango\Aliyun\Slb\Action\BaseAction;
 use Swango\Aliyun\Slb\JsonBuilder\CreateRulesJsonBuilder;
+use Swango\Aliyun\Slb\LocalHTTPListener;
 class CreateRules extends BaseAction {
     public function __construct(CreateRulesJsonBuilder $helper = null) {
         parent::__construct();
         $this->request->setQueryParameter('LoadBalancerId', $this->config['balancer_id']);
+        $this->request->setQueryParameter('ListenerPort', LocalHTTPListener::BALANCER_LISTENER_PORT);
         if (isset($helper)) {
             $this->request->setQueryParameter('RuleList', $helper->__toString());
         }
