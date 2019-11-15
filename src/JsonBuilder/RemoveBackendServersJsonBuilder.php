94fb1f758479c45be902f760f1fee0cdffdbb045
Tree <tree@brea.co>
2019-11-15 15:47:32 +0800

    bug fix

--- src/JsonBuilder/RemoveBackendServersJsonBuilder.php
+++ src/JsonBuilder/RemoveBackendServersJsonBuilder.php
@@ -5,9 +5,10 @@ class RemoveBackendServersJsonBuilder {
     public function __toString() {
         return \Json::encode($this->array);
     }
-    public function addServer(string $server_id): self {
+    public function addServer(string $server_id, int $port): self {
         $server = [];
         $server['ServerId'] = $server_id;
+        $server['Port'] = $port;
         $this->array[] = $server;
         return $this;
     }
