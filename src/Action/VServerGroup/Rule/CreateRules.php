6a0efad313135439e79db8edbe24cae51bebf778
Tree <522262763@qq.com>
2021-03-16 14:04:08 +0800

    fix create rule

--- src/Action/VServerGroup/Rule/CreateRules.php
+++ src/Action/VServerGroup/Rule/CreateRules.php
@@ -7,7 +7,8 @@ class CreateRules extends BaseAction {
     public function __construct(CreateRulesJsonBuilder $helper = null) {
         parent::__construct();
         $this->request->setQueryParameter('LoadBalancerId', $this->config['balancer_id']);
-        $this->request->setQueryParameter('ListenerPort', LocalHTTPListener::BALANCER_LISTENER_PORT);
+        $this->request->setQueryParameter('ListenerPort',
+            $this->config['balancer_listener_port'] ?? LocalHTTPListener::BALANCER_LISTENER_PORT);
         if (isset($helper)) {
             $this->request->setQueryParameter('RuleList', $helper->toString());
         }
