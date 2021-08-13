3daa334fbf1c74ddf98d941cf1b365511bb3c140
Tree <522262763@qq.com>
2021-08-13 14:53:49 +0800

    bug fix

--- src/JsonBuilder/CreateRulesJsonBuilder.php
+++ src/JsonBuilder/CreateRulesJsonBuilder.php
@@ -8,13 +8,14 @@ class CreateRulesJsonBuilder {
     public function addRule(string $role_name, ?string $host, ?string $url, $group_id): self {
         $rule = [];
         $rule['RuleName'] = $role_name;
+        if (! isset($host) && ! isset($url)) {
+            throw new \Swango\Aliyun\Slb\Exception\SLBException('invalid rule');
+        }
         if (isset($host)) {
             $rule['Domain'] = $host;
         }
         if (isset($url)) {
             $rule['Url'] = $url;
-        } else {
-            throw new \Swango\Aliyun\Slb\Exception\SLBException('invalid rule');
         }
         $rule['VServerGroupId'] = $group_id;
         $this->array[] = $rule;
